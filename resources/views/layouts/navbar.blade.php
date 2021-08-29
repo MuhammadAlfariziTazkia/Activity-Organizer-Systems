884a40f281a241ee7df8410b93dcf75d8dac35aa
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-08-29 19:14:42 +0700

    feat: add navigate to schedule and assignment system

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Assignment Systems</a>
+    <a class="navbar-brand" href="#">Activity Organizer Systems</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,10 +10,10 @@
           <a class="nav-link" href="#">Dashboard</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Schedule</a>
+          <a class="nav-link" href="{{url('schedule')}}">Schedule</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Assignment</a>
+          <a class="nav-link" href="{{url('assignment')}}">Assignment</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Exam</a>
