0a9f3e6e61f78f7415b0e2dde11af747fe006c2f
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-09-13 14:59:31 +0700

    feat: add mechanism to add presence

--- app/Http/Controllers/PresenceController.php
+++ app/Http/Controllers/PresenceController.php
@@ -27,6 +27,7 @@ class PresenceController extends Controller
     public function create()
     {
         //
+        return view('Presence.create');
     }
 
     /**
@@ -38,6 +39,23 @@ class PresenceController extends Controller
     public function store(Request $request)
     {
         //
+        $current_date = date('Y-m-d');
+        
+        $presence_screenshot = $request->file('presence_screenshot');
+        $pre = new Presence;
+        $pre->subject = $request->subject;
+        $pre->date = $request->date;
+        $pre->status = $request->status;
+
+        if($presence_screenshot){
+            $original_name = $presence_screenshot->getClientOriginalName();
+            $presence_screenshot_fn = $current_date.' '.$original_name;
+            $pre->presence_screenshot = $presence_screenshot_fn;
+            $presence_screenshot->move(public_path().'/images/Presence', $presence_screenshot_fn);
+        }
+
+        $pre->save();
+        return redirect('presence');
     }
 
     /**
