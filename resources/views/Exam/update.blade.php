20df4dbdc43d9d745b516a6a15db679781dfbd3c
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-09-13 10:49:57 +0700

    fix: method to PUT

--- resources/views/Exam/update.blade.php
+++ resources/views/Exam/update.blade.php
@@ -4,9 +4,9 @@
 
     <h1>Update Page</h1>
 
-    <form action="{{ url('exam') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ url('/exam/'.$exam->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="hidden" method="PUT" name="_method">
+        <input type="hidden" name="_method" value="PUT">
         <div class="row">
             <div class="col-md-4">
                 <div class="mb-3">
@@ -33,15 +33,35 @@
                     </select>
                 </div>
             </div>
+            @php
+                $datetime = $exam->datetime;
+                $datetime_exploded = explode(' ', $datetime);
+                $date = $datetime_exploded[0];
+                $time = $datetime_exploded[1];
+                $time = str_replace("(", "", $time);
+                $time = str_replace(")", "", $time);
+            @endphp
             <div class="col-md-4">
                 <div class="mb-3">
                     <label for="date" class="form-label">Date</label>
-                    <input name="date" type="text" class="form-control" id="date" required value="{{$exam->date}}">
+                    <input name="date" type="text" class="form-control" id="date" required value="{{$date}}">
                 </div>
                 <div class="mb-3">
                     <label for="time" class="form-label">Time</label>
-                    <input name="time" type="text" class="form-control" id="time" required value="{{$exam->time}}">
+                    <input name="time" type="text" class="form-control" id="time" required value="{{$time}}">
                 </div>
+                <label for="Status" class="form-label">Status</label>
+            <div class="input-group">
+                <select class="form-select mb-3" name="status" id="Status"
+                    aria-label="Example select with button addon">
+                    <option value="Not yet" @if ($exam->status == 'Not yet')
+                        selected
+                    @endif>Not yet</option>
+                    <option value="Completed" @if ($exam->status == 'Completed')
+                        selected
+                    @endif>Completed</option>
+                </select>
+            </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
